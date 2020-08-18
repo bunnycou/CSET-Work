858f32149c3b38a20b64e4938ff2a8946e21908c
Noah Cou <noahjcou@gmail.com>
2020-08-18 15:01:20 -0400

    first lab

---
+++ wk1L1.py
@@ -0,0 +1,56 @@
+import turtle
+
+#print table
+print("Print Table:")
+print("a  a^2  a^3\n1  1  1\n2  4  8\n3  9  27\n4  16  64")
+
+#small space
+print("")
+
+#calculate
+print("Calculate Expression:")
+print(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5))
+
+#draw the turtle four square
+turtle.showturtle()
+
+#drawing squares like
+# 4 1
+# 3 2
+#draw squares
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+#leaves turtle open until clicked
+turtle.exitonclick()
