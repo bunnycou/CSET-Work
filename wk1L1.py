ba7e9cb6f688d554b902f3a0a3f5e623491391c8
Noah Cou <noahjcou@gmail.com>
2020-08-19 10:09:13 -0400

    updated gitignore and wk1l1 as well as added readme

--- wk1L1.py
+++ wk1L1.py
@@ -1,23 +1,23 @@
 import turtle
 
-#print table
+# print table
 print("Print Table:")
 print("a  a^2  a^3\n1  1  1\n2  4  8\n3  9  27\n4  16  64")
 
-#small space
+# small space
 print("")
 
-#calculate
+# calculate
 print("Calculate Expression:")
 print(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5))
 
-#draw the turtle four square
+# draw the turtle four square
 turtle.showturtle()
 
 #drawing squares like
 # 4 1
 # 3 2
-#draw squares
+# draw squares
 turtle.left(90)
 turtle.forward(100)
 turtle.right(90)
@@ -52,5 +52,5 @@ turtle.forward(100)
 turtle.right(90)
 turtle.forward(100)
 
-#leaves turtle open until clicked
+# leaves turtle open until clicked
 turtle.exitonclick()
