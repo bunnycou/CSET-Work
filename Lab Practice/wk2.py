18e35d29b91ec22689b4c27b6ad6afd8b37c2c00
Noah Cou <noahjcou@gmail.com>
2020-08-25 13:24:53 -0400

    wk2 lab practice finished

---
+++ Lab Practice/wk2.py
@@ -0,0 +1,36 @@
+# get pi from the math module
+from  math import pi
+
+# Question 1
+# Read celsius input and output fahrenheit
+print("Question 1\n")
+
+ce = eval(input("Input a degree in celsius: "))
+fa = ((9/5) * ce) + 32
+
+print(f"{ce} in celsius is {fa} in fahrenheit")
+
+print("-----")
+
+# Question 2
+# Read radius and length input and output area and volume of a cylinder
+print("Question 2\n")
+
+rad = eval(input("Input radius of a cylinder: "))
+len = eval(input("Input length of a cylinder: "))
+
+area = (rad**2) * pi
+vol = area * len
+
+print(f"The area is {round(area, 2)}\nThe colume is {round(vol, 2)}")
+
+print("-----")
+
+# Question 3
+# Read feet and convert into meters
+print("Question 3\n")
+
+ft = eval(input("Input feet: "))
+me = ft * 0.305
+
+print(f"{ft} feet is {me} meters")
