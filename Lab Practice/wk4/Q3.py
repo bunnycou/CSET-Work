0d586c60b15019343835e687f5df516e3745abe8
Noah Cou <noahjcou@gmail.com>
2020-09-08 14:02:24 -0400

    added weight groups to q3

--- Lab Practice/wk4/Q3.py
+++ Lab Practice/wk4/Q3.py
@@ -4,8 +4,8 @@
 # Display BMI
 
 # collect weight and height
-weight = eval(input("How much do you weigh? "))
-height = eval(input("How tall are you? "))
+weight = eval(input("How much do you weigh in pounds? "))
+height = eval(input("How tall are you in inches? "))
 
 # constants for conversion
 WKG = 0.45359237
@@ -18,3 +18,11 @@ m = height * HM
 bmi = kg / m**2
 
 print(f"Your BMI is: {round(bmi, 1)}")
+if bmi < 18.5:
+    print("You are underweight")
+elif bmi < 25:
+    print("You are at a normal weight")
+elif bmi < 30:
+    print("You are overweight")
+else:
+    print("You are obese")
