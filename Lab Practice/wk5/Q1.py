d0a2fbc2270517d564c34f3525e93bff4734260e
Noah Cou <noahjcou@gmail.com>
2020-09-15 13:11:04 -0400

    finished Q1

--- Lab Practice/wk5/Q1.py
+++ Lab Practice/wk5/Q1.py
@@ -1,4 +1,17 @@
 ##############
 # Question 1 #
 ##############
-# function practice
+# function practice of finding the max number
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+def main():
+    i = 11
+    j = 7
+    k = max(i, j)
+    print(f"The largest of {i} and {j} is {k}")
+
+main()
