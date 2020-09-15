0248d52d26a7d10f7117cbed2322b440a84e9ba0
Noah Cou <noahjcou@gmail.com>
2020-09-15 14:22:31 -0400

    finished q6

--- Lab Practice/wk5/Q6.py
+++ Lab Practice/wk5/Q6.py
@@ -2,3 +2,13 @@
 # Question 6 #
 ##############
 # default argument practice
+
+def printArea(width = 1, height = 2):
+    area = width * height
+    print(f"\nWidth: {width}\nHeight: {height}\nArea: {area}")
+
+printArea()
+printArea(4, 2.5)
+printArea(height = 5, width = 3)
+printArea(width = 1.2)
+printArea(height = 6.2)
