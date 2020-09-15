5c759bd926800831556bae2e80332b00ef44dfa2
Noah Cou <noahjcou@gmail.com>
2020-09-15 14:40:39 -0400

    finished q8

--- Lab Practice/wk5/Q8.py
+++ Lab Practice/wk5/Q8.py
@@ -2,3 +2,15 @@
 # Question 8 #
 ##############
 # generate random characters
+
+import randomCharacter
+
+NUMBER_OF_CHARS = 175
+CHAR_PER_LINE = 25
+
+# print a lot of characters
+for x in range(NUMBER_OF_CHARS):
+    print(randomCharacter.randomLower(), end = " ")
+    if (x + 1) % CHAR_PER_LINE == 0:
+        print()
+
