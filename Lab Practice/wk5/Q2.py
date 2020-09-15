cc0980e8687ad847c6b1c41fdd975e2da5c85092
Noah Cou <noahjcou@gmail.com>
2020-09-15 13:24:04 -0400

    finished q2

--- Lab Practice/wk5/Q2.py
+++ Lab Practice/wk5/Q2.py
@@ -2,3 +2,22 @@
 # Question 2 #
 ##############
 # more function practice
+
+# function to give letter grade
+def getGrade(score):
+    if score >= 90.0:
+        return 'A'
+    elif score >= 80.0:
+        return 'B'
+    elif score >= 70.0:
+        return 'C'
+    elif score >= 60.0:
+        return 'D'
+    else:
+        return 'F'
+
+def main():
+    score = eval(input("Enter the percent grade: "))
+    print(f"Their grade is {getGrade(score)}")
+
+main()
