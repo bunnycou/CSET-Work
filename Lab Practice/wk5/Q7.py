ade9f60fb02ccfc0249e1ca4105aef0418d0b889
Noah Cou <noahjcou@gmail.com>
2020-09-15 14:27:52 -0400

    finished q7

--- Lab Practice/wk5/Q7.py
+++ Lab Practice/wk5/Q7.py
@@ -2,3 +2,12 @@
 # Question 7 #
 ##############
 # returning multiple values
+
+def sort(a, b):
+    if a < b:
+        return a, b
+    else:
+        return b, a
+
+x, y = sort(5, 2)
+print(f"First number is {x}\nSecond number is {y}")
