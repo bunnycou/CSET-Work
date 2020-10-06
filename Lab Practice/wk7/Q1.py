967ca9f8dca4af9291d5bf213127148c391339e4
Noah Cou <noahjcou@gmail.com>
2020-10-06 13:08:14 -0400

    added spacing and first commit to main

--- Lab Practice/wk7/Q1.py
+++ Lab Practice/wk7/Q1.py
@@ -2,3 +2,4 @@
 # Question 1 #
 ##############
 # Processing Event example to demonstrate the basis of event-driven programming
+
--- Lab Practice/wk7/Q4.py
+++ Lab Practice/wk7/Q4.py
@@ -2,3 +2,4 @@
 # Question 4 #
 ##############
 # Program that shows how to use the canvas widget
+
