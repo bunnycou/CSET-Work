72207b73ff1ae3a96d3d1b45b98cd140529a3082
Noah Cou <noahjcou@gmail.com>
2020-10-06 14:41:39 -0400

    partially finished 4 and finished 5 although it is messy

--- Lab Practice/wk7/Q4.py
+++ Lab Practice/wk7/Q4.py
@@ -3,3 +3,20 @@
 ##############
 # Program that shows how to use the canvas widget
 
+from tkinter import *
+
+class CavasDemo:
+    def __init__(self):
+        window = Tk()
+        window.title("Canvas Demo")
+
+        frame1 = Frame(window)
+        frame1.pack()
+
+        canvas = Canvas(frame1, bg = "white", height = 300, width = 300)
+        rect = canvas.create_rectangle(10, 10, 30, 20)
+
+        canvas.pack()
+        window.mainloop()
+
+CanvasDemo()
--- Lab Practice/wk7/Q5.py
+++ Lab Practice/wk7/Q5.py
@@ -3,3 +3,48 @@
 ##############
 # GUI Loan Calculator
 
+from tkinter import Tk, Label, Entry, Button, StringVar, RIGHT, W, E
+
+class LoanCalculatoor:
+    def __init__(self):
+        window = Tk()
+        window.title("Loan Calculator")
+
+        Label(window, text = "Annual Interest Rate").grid(row = 1, column = 1, sticky = W)
+        Label(window, text = "Number of Years").grid(row = 2, column = 1, sticky = W)
+        Label(window, text = "Loan Amount").grid(row = 3, column = 1, sticky = W)
+        Label(window, text = "Monthly Payment").grid(row = 4, column = 1, sticky = W)
+        Label(window, text = "Total Payment").grid(row = 5, column = 1, sticky = W)
+
+        self.annualInterestRateVar = StringVar()
+        Entry(window, textvariable = self.annualInterestRateVar, justify = RIGHT).grid(row = 1, column = 2)
+        
+        self.numberofYearsVar = StringVar()
+        Entry(window, textvariable = self.numberofYearsVar, justify = RIGHT).grid(row = 2, column = 2)
+        
+        self.loanAmountVar = StringVar()
+        Entry(window, textvariable = self.loanAmountVar, justify = RIGHT).grid(row = 3, column = 2)
+        
+        self.monthlyPaymentVar = StringVar()
+        lblMonthlyPayment = Label(window, textvariable = self.monthlyPaymentVar).grid(row = 4, column = 2, sticky = E)
+
+        self.totalPaymentVar = StringVar()
+        lblTotalPayment = Label(window, textvariable = self.totalPaymentVar).grid(row = 5, column = 2, sticky = E)
+
+        btbComputePayment = Button(window, text = "Compute Payment", command = self.computePayment).grid(row = 6, column = 2, sticky = E)
+
+        window.mainloop()
+
+    def computePayment(self):
+        monthlyPayment = self.getmonthlyPayment(float(self.loanAmountVar.get()), float(self.annualInterestRateVar.get()) / 1200, int(self.numberofYearsVar.get()))
+        self.monthlyPaymentVar.set(format(monthlyPayment, "10.2f"))
+        totalPayment = float(self.monthlyPaymentVar.get()) * 12 * int(self.numberofYearsVar.get())
+        self.totalPaymentVar.set(format(totalPayment, "10.2f"))
+
+    def getmonthlyPayment(self, loanAmount, monthlyInterestRate, numberOfYears):
+        monthlyPayment = loanAmount * monthlyInterestRate / ( 1 - 1 / (1+ monthlyInterestRate) ** (numberOfYears * 12))
+        return monthlyPayment
+
+LoanCalculatoor()
+
+
