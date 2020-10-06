d01952875667ff9206b0a0a469ba8d41133e8f29
Noah Cou <noahjcou@gmail.com>
2020-10-06 13:18:35 -0400

    put code in wrong file

--- Lab Practice/wk7/Q1.py
+++ Lab Practice/wk7/Q1.py
@@ -3,3 +3,30 @@
 ##############
 # Processing Event example to demonstrate the basis of event-driven programming
 
+# import tkinter
+from tkinter import Tk, Button
+
+class processButtonEvent:
+    def __init__(self):
+
+        # create the window
+        window = Tk()
+
+        # create widgets
+        btOK = Button(window, text = "OK", fg = "red", command = self.processOK)
+        btCancel = Button(window, text = "Cancel", bg = "Yellow", command = self.processCancel)
+        
+        # place widgets
+        btOK.pack()
+        btCancel.pack()
+
+        # loop event
+        window.mainloop()
+
+    def processOK(self):
+        print("OK Button is Clicked")
+
+    def processCancel(self):
+        print("Cancel Button is Clicked")
+
+processButtonEvent()
--- Lab Practice/wk7/Q4.py
+++ Lab Practice/wk7/Q4.py
@@ -3,30 +3,3 @@
 ##############
 # Program that shows how to use the canvas widget
 
-# import tkinter
-from tkinter import Tk, Button
-
-class processButtonEvent:
-    def __init__(self):
-
-        # create the window
-        window = Tk()
-
-        # create widgets
-        btOK = Button(window, text = "OK", fg = "red", command = self.processOK)
-        btCancel = Button(window, text = "Cancel", bg = "Yellow", command = self.processCancel)
-        
-        # place widgets
-        btOK.pack()
-        btCancel.pack()
-
-        # loop event
-        window.mainloop()
-
-    def processOK(self):
-        print("OK Button is Clicked")
-
-    def processCancel(self):
-        print("Cancel Button is Clicked")
-
-processButtonEvent()
