d96da5841615bbc3288f9d173e0f1e835467330e
Noah Cou <noahjcou@gmail.com>
2020-10-13 13:45:36 -0400

    finished most of q4 and added randomchar to wk 9

--- Lab Practice/wk9/Q4.py
+++ Lab Practice/wk9/Q4.py
@@ -2,3 +2,20 @@
 # Question 4 #
 ##############
 # Count the occurence of letters
+
+# import method for creating random characters
+from randomCharacter import randomLower
+
+# make a list of random characters
+chars = []
+
+for x in range(100):
+    chars.append(randomLower())
+
+# display all of the characters
+for x in range(len(chars)):
+    if (x + 1) % 20 == 0:
+        print(chars[x])
+    else:
+        print(chars[x], end = " ")
+
